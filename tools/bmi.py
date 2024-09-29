2caa4adee5a89f3bfea8e23f63e986bf463107e0
saito-zenn <7kasugano@gmail.com>
2024-09-29 20:21:56 +0900

    bmi
    bmi

---
+++ tools/bmi.py
@@ -0,0 +1,15 @@
+@tool
+def calculate_bmi(height, weight):
+  """BMIを計算する関数
+
+  Args:
+    height: 身長 (m)
+    weight: 体重 (kg)
+
+  Returns:
+    BMI: 体重指数
+  """
+
+  bmi = weight / (height ** 2)
+  return bmi
+
